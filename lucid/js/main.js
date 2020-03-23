84c68337df15bebdb19968316ebd94d81def9d1c
Roma <rzeylik@gmail.com>
2020-03-23 21:10:57 +0200

    Add desktop version of site "lucid"

---
+++ lucid/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  $('.comments-slider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: false,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 5000,
+  });
+
+});
